7fcf73146b21c672b75dd14dbdca2e9a8fdc3817
exumbra_insolem <lesguillemets@github.fake>
2013-11-14 19:39:06 +0900

    python timing

---
+++ Scripts/15Nov2013.timing.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import time
+
+x = time.time()
+for i in xrange(1000):
+    for j in xrange(1000):
+        a=3
+        b=5
+        if a < b:
+            pass
+        else:
+            pass
+y = time.time()
+print y-x
+
+x = time.time()
+def g():
+    for i in xrange(1000):
+        for j in xrange(1000):
+            a=3
+            b=5
+            if a < b:
+                pass
+            else:
+                pass
+y = time.time()
+print y-x
+
+x2 = time.time()
+g()
+y2 = time.time()
+print y2-x2
+
